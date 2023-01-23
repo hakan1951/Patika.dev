5d937f7bfc333e97b6d9d0541e8718f2c65b6a45
hakan1951 <hakantasariztech@gmail.com>
2023-01-23 20:33:24 +0300

    sod added.

---
+++ Backend-Beginner/ArmstrongRelated-1/src/DigitSum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+//Sum of digits.
+public class DigitSum {
+    public static void main(String[] args) {
+        System.out.print("Enter number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int result = 0;
+        int temp = number;
+
+        do{
+            result += temp%10;
+            temp /=10;
+        } while (temp > 0);
+        System.out.format("Sum of digits of %d : %d", number, result);
+    }
+}
