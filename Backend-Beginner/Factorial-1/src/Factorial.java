26baee4f1143210e78028fe31de16942a12b2a7c
hakan1951 <hakantasariztech@gmail.com>
2023-01-23 17:36:22 +0300

    Factorial-1 added.

---
+++ Backend-Beginner/Factorial-1/src/Factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+/*
+    Combination formula
+    C(n,r) = n! / (r! * (n-r)!)
+*/
+public class Factorial {
+    public static int factorial(int number) {
+        int total = 1;
+        if (number == 0) {
+            return total;
+        }
+        for (int i = 1; i <= number; i++) {
+            total *= i;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a combination C(n,r): ");
+        System.out.print("n: ");
+        int n = sc.nextInt();
+        System.out.print("r: ");
+        int r = sc.nextInt();
+        int combination = factorial(n) / (factorial(r) * (factorial(n-r)));
+        System.out.format("C(%d,%d) : %d" , n, r, combination);
+
+    }
+}
