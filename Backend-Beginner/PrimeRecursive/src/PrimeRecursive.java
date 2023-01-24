c7b86cb7e58014ce97a85a63db80a585d031affb
hakan1951 <hakantasariztech@gmail.com>
2023-01-25 00:53:15 +0300

    PrimeRecursive updated.

--- Backend-Beginner/PrimeRecursive/src/PrimeRecursive.java
+++ Backend-Beginner/PrimeRecursive/src/PrimeRecursive.java
@@ -4,6 +4,7 @@ public class PrimeRecursive {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
+        sc.close();
         int number = sc.nextInt();
         if (isPrime(number, number/2)){
             System.out.println(number + " is a prime number");
