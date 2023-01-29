92103f9aec5feb7b2a37c8f52f4b0181e0dc9ef5
hakan1951 <hakantasariztech@gmail.com>
2023-01-30 00:43:28 +0300

    PalindromeString added.

---
+++ Backend-Beginner/PalindromeString/src/PalindromeString.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = sc.nextLine();
+        if (isPalindrome(input)){
+            System.out.format("%s is palindrome.", input);
+        }
+    }
+
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
