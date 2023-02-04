17bc3fb50cbf492ce959646c808e94f956f6c061
hakan1951 <hakantasariztech@gmail.com>
2023-02-04 22:24:55 +0300

    Create hw6.sql

---
+++ Backend-Intermediate/Sql/hw6.sql
@@ -0,0 +1,18 @@
+--1--
+Select AVG(rental_rate)
+From film;
+
+--2--
+Select Count(*)
+From film
+Where film.title LIKE 'C%';
+
+--3--
+Select MAX(film.length)
+From film
+Where film.rental_rate=0.99;
+
+--4--
+Select Count(Distinct replacement_cost)
+From film
+Where film.length>150
